3f0ca46b0828d8c82bc50298ff48a2257c1d4ebc
b047ff <b047ff@yamaguchi-u.ac.jp>
2025-01-30 14:52:04 +0900

    Remove the echo command

--- deploy.sh
+++ deploy.sh
@@ -15,4 +15,3 @@ cp -R "$SOURCE_DIR"/* "$TARGET_DIR"
 # Jekyll ビルドを無効にするために .nojekyll ファイルを追加
 touch "$TARGET_DIR/.nojekyll"
 ls -la docs/.nojekyll
-echo ".nojekyll file created at $TARGET_DIR/.nojekyll"
